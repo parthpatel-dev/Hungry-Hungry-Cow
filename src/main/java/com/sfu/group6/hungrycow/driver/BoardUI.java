9c6c909af7c0c8ed4f8b8cbe12fd90dcce20e310
Devgroup-Leon <leonzhu2011@gmail.com>
2022-11-02 16:15:04 -0700

    created new branch with updated boardUI structure

--- src/main/java/com/sfu/group6/hungrycow/driver/BoardUI.java
+++ src/main/java/com/sfu/group6/hungrycow/driver/BoardUI.java
@@ -1,6 +1,7 @@
 package com.sfu.group6.hungrycow.driver;
 
 import com.sfu.group6.hungrycow.driver.tile.TileHandler;
+import com.sfu.group6.hungrycow.driver.Board; 
 
 import javax.swing.*;
 import java.awt.*;
@@ -51,9 +52,34 @@ public class BoardUI extends JPanel implements Runnable{
         g2.setColor(Color.white);
         g2.fillRect(100, 100, tileSize, tileSize);
 
-        tileHandler.drawTile(g2);
+        
+        if (Board.inGame()) {
+            playGame(g2d); //For drawing the entities
+        } else {
+            showIntroScreen(g2d);
+        }
 
         g2.dispose();
      }
+}
 
+private void playGame(Graphics2D g2d) {
+	 tileHandler.drawTile(g2);
+    if (Board.status() == gameOver) {
+        gameOverScreen(g2d);
+    } else if(Board.status() == victory){
+    	victoryScreen(g2d);
+    }else {
+        drawPlayer(g2d);
+        drawEnemy(g2d);
+        drawPunishment(g2d);
+        drawRewards(g2d);
+        drawBonusReward(g2d);
+        drawBarriers(g2d);
+        if(Board.reward() == true) {
+        	drawOpenedExit(g2d);
+        } else {
+        	drawClosedExit(g2d);
+        }
+    }
 }
