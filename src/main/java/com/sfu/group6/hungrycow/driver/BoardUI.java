2b4f1a5e3678d02392021615ee356b5092ff83f8
Devgroup-Leon <leonzhu2011@gmail.com>
2022-11-02 22:19:52 -0700

    Added EvenListerner

--- src/main/java/com/sfu/group6/hungrycow/driver/BoardUI.java
+++ src/main/java/com/sfu/group6/hungrycow/driver/BoardUI.java
@@ -2,15 +2,18 @@ package com.sfu.group6.hungrycow.driver;
 
 import com.sfu.group6.hungrycow.driver.tile.TileHandler;
 import com.sfu.group6.hungrycow.driver.Board; 
+import com.sfu.group6.hungrycow.control.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class BoardUI extends JPanel implements Runnable{
+public class BoardUI extends JPanel implements Runnable, KeyListener {
 
 	Board board;
     final int defaultTileSize = 16;
     final int scale = 3;
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
     public boolean startButtonPress = true;
     public final int tileSize = defaultTileSize * scale;
     public final int numOfTilesHorizontal = 20;
@@ -21,8 +24,9 @@ public class BoardUI extends JPanel implements Runnable{
     public BoardUI() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
-        Board = boardFactory.createBoard();
+        //Board = boardFactory.createBoard();
         board = Board.builder().build();
+        //board.getPlayer().getPosition().getX();
     }
 
     Thread gameThread;
@@ -32,26 +36,35 @@ public class BoardUI extends JPanel implements Runnable{
     }
 
     TileHandler tileHandler = new TileHandler(this);
-
+    
 
     @Override
     public void run() {
 
         while(gameThread != null) {
-
+        	
             update();
             repaint();
+            
         }
     }
-
+    
     public void update() {
-
+    	if(upPressed == true) {
+    		 board.tickBoardState(Direction.UP);
+    	} else if(downPressed == true) {
+    		board.tickBoardState(Direction.DOWN);
+    	} else if(leftPressed == true) {
+    		board.tickBoardState(Direction.LEFT);
+    	} else if(rightPressed == true) {
+    		board.tickBoardState(Direction.RIGHT);
+    	}
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
-
+        
         g2.setColor(Color.white);
         g2.fillRect(100, 100, tileSize, tileSize);
 
@@ -68,21 +81,53 @@ public class BoardUI extends JPanel implements Runnable{
 
 private void playGame(Graphics2D g2) {
 	 tileHandler.drawTile(g2);
-    if (Board.isGameOver() == true) {
+	 drawScore(g2);
+    if (board.isGameOver() == true) {
         gameOverScreen(g2);
-    } else if(Board.isGameOver() == true){
+    } else if(board.isGameOver() == false){
     	victoryScreen(g2);
     }else {
         drawPlayer(g2);
         drawEnemy(g2);
-        drawPunishment(g2);
-        drawRewards(g2);
-        drawBonusReward(g2);
-        drawBarriers(g2);
+        /*
+         * drawPunishment
+         * drawBonusReward
+         * drawReward
+         * 
+        */
         if(Board.reward() == true) {
         	drawOpenedExit(g2);
         } else {
         	drawClosedExit(g2);
         }
     }
+}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	switch(e.getKeyCode()) {
+	case 37: leftPressed = true; //For arrow key left
+	        break;
+	case 38: upPressed = true; //For arrow key up
+	        break;
+	case 39: downPressed = true; //For arrow key down
+	        break;
+	case 40: rightPressed = true; //For arrow key right
+	        break;
+	}
+
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+
+}
 }
