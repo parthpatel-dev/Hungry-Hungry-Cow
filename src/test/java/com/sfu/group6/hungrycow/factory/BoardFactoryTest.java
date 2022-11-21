6f17036dc09fa869e0aa05427e335b96e180f488
Ryan Leung <ryanleung1004@gmail.com>
2022-11-21 14:32:02 -0800

    Code review changes

--- src/test/java/com/sfu/group6/hungrycow/factory/BoardFactoryTest.java
+++ src/test/java/com/sfu/group6/hungrycow/factory/BoardFactoryTest.java
@@ -1,13 +1,11 @@
 package com.sfu.group6.hungrycow.factory;
 
 import com.sfu.group6.hungrycow.control.Direction;
-import com.sfu.group6.hungrycow.control.Position;
 import com.sfu.group6.hungrycow.driver.Board;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
-import java.util.Dictionary;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,19 +13,16 @@ public class BoardFactoryTest {
     private final HungryCowBoardFactory boardFactory = new HungryCowBoardFactory();
     private final HungryCowAnimateFactory animateFactory = new HungryCowAnimateFactory();
     private final HungryCowInanimateFactory inanimateFactory = new HungryCowInanimateFactory();
-    private int[][] array2D = new int[11][11];
+    private final int[][] array2D = new int[11][11];
 
     private Board fixture;
 
 
     @BeforeEach
-    void setup()
-    {
+    void setup() {
         // Should set all spaces to 0
-        for (int y = 0; y  < array2D.length; y++)
-        {
-            for (int x = 0; x < array2D[y].length; x++)
-            {
+        for (int y = 0; y < array2D.length; y++) {
+            for (int x = 0; x < array2D[y].length; x++) {
                 array2D[x][y] = 0;
             }
         }
@@ -35,72 +30,74 @@ public class BoardFactoryTest {
         array2D[0][0] = 17;
         array2D[0][1] = 1;
         array2D[10][10] = 18;
-        fixture = boardFactory.createBoard(array2D,animateFactory,inanimateFactory);
+        fixture = boardFactory.createBoard(array2D,
+                                           animateFactory,
+                                           inanimateFactory);
 
     }
 
     @Test
-    void shouldMakeBoardWidthFromFactory()
-    {
+    void shouldMakeBoardWidthFromFactory() {
         assertThat(fixture.getWidth()).isEqualTo(11);
     }
 
     @Test
-    void shouldMakeBoardHeightFromFactory()
-    {
+    void shouldMakeBoardHeightFromFactory() {
         assertThat(fixture.getHeight()).isEqualTo(11);
     }
 
     @Test
-    void shouldMakeBoardStartSpaceFromFactory()
-    {
-        assertThat(fixture.getStartSpace().getX()).isEqualTo(0);
-        assertThat(fixture.getStartSpace().getY()).isEqualTo(0);
+    void shouldMakeBoardStartSpaceFromFactory() {
+        assertThat(fixture.getStartSpace()
+                          .getX()).isEqualTo(0);
+        assertThat(fixture.getStartSpace()
+                          .getY()).isEqualTo(0);
     }
 
     @Test
-    void shouldMakeBoardEndSpaceFromFactory()
-    {
-        assertThat(fixture.getEndSpace().getX()).isEqualTo(10);
-        assertThat(fixture.getEndSpace().getY()).isEqualTo(10);
+    void shouldMakeBoardEndSpaceFromFactory() {
+        assertThat(fixture.getEndSpace()
+                          .getX()).isEqualTo(10);
+        assertThat(fixture.getEndSpace()
+                          .getY()).isEqualTo(10);
     }
 
     @Test
-    void shouldMakeBoardBarriersFromFactory()
-    {
+    void shouldMakeBoardBarriersFromFactory() {
         assertThat(fixture.getBarriers()).isEqualTo(Collections.emptySet());
     }
 
     @Test
-    void shouldMakeBoardPlayerFromFactory()
-    {
-        assertThat(fixture.getPlayer().getPosition().getX()).isEqualTo(1);
-        assertThat(fixture.getPlayer().getPosition().getY()).isEqualTo(0);
-        assertThat(fixture.getPlayer().getFacingDirection()).isEqualTo(Direction.RIGHT);
-        assertThat(fixture.getPlayer().getScore()).isEqualTo(HungryCowAnimateFactory.INITIAL_PLAYER_SCORE);
+    void shouldMakeBoardPlayerFromFactory() {
+        assertThat(fixture.getPlayer()
+                          .getPosition()
+                          .getX()).isEqualTo(1);
+        assertThat(fixture.getPlayer()
+                          .getPosition()
+                          .getY()).isEqualTo(0);
+        assertThat(fixture.getPlayer()
+                          .getFacingDirection()).isEqualTo(Direction.RIGHT);
+        assertThat(fixture.getPlayer()
+                          .getScore()).isEqualTo(HungryCowAnimateFactory.INITIAL_PLAYER_SCORE);
     }
 
     @Test
-    void shouldMakeBoardEnemiesFromFactory()
-    {
+    void shouldMakeBoardEnemiesFromFactory() {
         assertThat(fixture.getEnemies()).isEqualTo(Collections.emptyList());
     }
 
     @Test
-    void shouldMakeBoardObjectivesFromFactory()
-    {
+    void shouldMakeBoardObjectivesFromFactory() {
         assertThat(fixture.getObjectives()).isEqualTo(Collections.emptyList());
     }
 
     @Test
-    void shouldMakeBoardPunishmentsFromFactory()
-    {
+    void shouldMakeBoardPunishmentsFromFactory() {
         assertThat(fixture.getPunishments()).isEqualTo(Collections.emptyList());
     }
 
     @Test
-    void shouldMakeBoardBonusFromFactory()
-    {
+    void shouldMakeBoardBonusFromFactory() {
         assertThat(fixture.getBonus()).isEqualTo(Collections.emptyList());
     }
 
--- src/test/java/com/sfu/group6/hungrycow/factory/InanimateFactoryTest.java
+++ src/test/java/com/sfu/group6/hungrycow/factory/InanimateFactoryTest.java
@@ -13,37 +13,42 @@ public class InanimateFactoryTest {
     private HungryCowInanimateFactory fixture;
 
     @BeforeEach
-    void setup()
-    {
+    void setup() {
         fixture = new HungryCowInanimateFactory();
 
     }
 
     @Test
-    void shouldMakeRegularRewardFromFactory()
-    {
-        RegularReward factoryRegularReward = fixture.makeRegularReward(0,0);
-        assertThat(factoryRegularReward.getPosition().getX()).isEqualTo(0);
-        assertThat(factoryRegularReward.getPosition().getY()).isEqualTo(0);
+    void shouldMakeRegularRewardFromFactory() {
+        RegularReward factoryRegularReward = fixture.makeRegularReward(0,
+                                                                       0);
+        assertThat(factoryRegularReward.getPosition()
+                                       .getX()).isEqualTo(0);
+        assertThat(factoryRegularReward.getPosition()
+                                       .getY()).isEqualTo(0);
         assertThat(factoryRegularReward.getValue()).isEqualTo(HungryCowInanimateFactory.REGULAR_REWARD_VALUE);
     }
 
     @Test
-    void shouldMakeBonusRewardFromFactory()
-    {
-        BonusReward factoryBonusReward = fixture.makeBonusReward(0,0);
+    void shouldMakeBonusRewardFromFactory() {
+        BonusReward factoryBonusReward = fixture.makeBonusReward(0,
+                                                                 0);
 
-        assertThat(factoryBonusReward.getPosition().getX()).isEqualTo(0);
-        assertThat(factoryBonusReward.getPosition().getY()).isEqualTo(0);
-        assertThat(factoryBonusReward.getValue()).isEqualTo(HungryCowInanimateFactory.BONUS_REWARD_VALUE); //change this to defined number like in InanimateFactory
+        assertThat(factoryBonusReward.getPosition()
+                                     .getX()).isEqualTo(0);
+        assertThat(factoryBonusReward.getPosition()
+                                     .getY()).isEqualTo(0);
+        assertThat(factoryBonusReward.getValue()).isEqualTo(HungryCowInanimateFactory.BONUS_REWARD_VALUE);
     }
 
     @Test
-    void shouldMakePunishmentFromFactory()
-    {
-        Punishment factoryPunishment = fixture.makePunishment(0,0);
-        assertThat(factoryPunishment.getPosition().getX()).isEqualTo(0);
-        assertThat(factoryPunishment.getPosition().getY()).isEqualTo(0);
+    void shouldMakePunishmentFromFactory() {
+        Punishment factoryPunishment = fixture.makePunishment(0,
+                                                              0);
+        assertThat(factoryPunishment.getPosition()
+                                    .getX()).isEqualTo(0);
+        assertThat(factoryPunishment.getPosition()
+                                    .getY()).isEqualTo(0);
         assertThat(factoryPunishment.getValue()).isEqualTo(HungryCowInanimateFactory.PUNISHMENT_VALUE);
     }
 }
