2e5c0f4f40e56afcc337030ce4adc2edfdd50c09
Lawrence <lwa117@sfu.ca>
2022-11-25 17:34:43 -0800

    Added more test cases for 100% line coverage

--- src/test/java/com/sfu/group6/hungrycow/factory/BoardFactoryTest.java
+++ src/test/java/com/sfu/group6/hungrycow/factory/BoardFactoryTest.java
@@ -1,11 +1,16 @@
 package com.sfu.group6.hungrycow.factory;
 
 import com.sfu.group6.hungrycow.control.Direction;
+import com.sfu.group6.hungrycow.control.Position;
 import com.sfu.group6.hungrycow.driver.Board;
+import com.sfu.group6.hungrycow.model.inanimate.BonusReward;
+import com.sfu.group6.hungrycow.model.inanimate.Punishment;
+import com.sfu.group6.hungrycow.model.inanimate.RegularReward;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,62 +18,78 @@ public class BoardFactoryTest {
     private final HungryCowBoardFactory boardFactory = new HungryCowBoardFactory();
     private final HungryCowAnimateFactory animateFactory = new HungryCowAnimateFactory();
     private final HungryCowInanimateFactory inanimateFactory = new HungryCowInanimateFactory();
-    private final int[][] array2D = new int[11][11];
+    private final int[][] emptyArray2D = {
+            {0, 1, 0, 0},
+            {0, 0, 17, 0},
+            {0, 0, 0, 18},
+            {0, 0, 0, 0}
+    };
+
+    private final int[][] filledArray2D = {
+            {1, 2, 3, 4, 5},
+            {6, 7, 8, 9, 10},
+            {11, 12, 13, 14, 15},
+            {16, 17, 18, 0, 0}
+    };
 
     private Board fixture;
 
 
     @BeforeEach
     void setup() {
-        // Should set all spaces to 0
-        for (int y = 0; y < array2D.length; y++) {
-            for (int x = 0; x < array2D[y].length; x++) {
-                array2D[x][y] = 0;
-            }
-        }
-        // Only sets the start and end space
-        array2D[0][0] = 17;
-        array2D[0][1] = 1;
-        array2D[10][10] = 18;
-        fixture = boardFactory.createBoard(array2D,
-                                           animateFactory,
-                                           inanimateFactory);
-
     }
 
     @Test
     void shouldMakeBoardWidthFromFactory() {
-        assertThat(fixture.getWidth()).isEqualTo(11);
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
+        assertThat(fixture.getWidth()).isEqualTo(4);
     }
 
     @Test
     void shouldMakeBoardHeightFromFactory() {
-        assertThat(fixture.getHeight()).isEqualTo(11);
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
+        assertThat(fixture.getHeight()).isEqualTo(4);
     }
 
     @Test
     void shouldMakeBoardStartSpaceFromFactory() {
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
         assertThat(fixture.getStartSpace()
-                          .getX()).isEqualTo(0);
+                          .getX()).isEqualTo(2);
         assertThat(fixture.getStartSpace()
-                          .getY()).isEqualTo(0);
+                          .getY()).isEqualTo(1);
     }
 
     @Test
     void shouldMakeBoardEndSpaceFromFactory() {
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
         assertThat(fixture.getEndSpace()
-                          .getX()).isEqualTo(10);
+                          .getX()).isEqualTo(3);
         assertThat(fixture.getEndSpace()
-                          .getY()).isEqualTo(10);
+                          .getY()).isEqualTo(2);
     }
 
     @Test
-    void shouldMakeBoardBarriersFromFactory() {
+    void shouldMakeBoardNoBarriersFromFactory() {
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
         assertThat(fixture.getBarriers()).isEqualTo(Collections.emptySet());
     }
 
     @Test
     void shouldMakeBoardPlayerFromFactory() {
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
         assertThat(fixture.getPlayer()
                           .getPosition()
                           .getX()).isEqualTo(1);
@@ -82,23 +103,119 @@ public class BoardFactoryTest {
     }
 
     @Test
-    void shouldMakeBoardEnemiesFromFactory() {
+    void shouldMakeBoardNoEnemiesFromFactory() {
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
         assertThat(fixture.getEnemies()).isEqualTo(Collections.emptyList());
     }
 
     @Test
-    void shouldMakeBoardObjectivesFromFactory() {
+    void shouldMakeBoardNoObjectivesFromFactory() {
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
         assertThat(fixture.getObjectives()).isEqualTo(Collections.emptyList());
     }
 
     @Test
-    void shouldMakeBoardPunishmentsFromFactory() {
+    void shouldMakeBoardNoPunishmentsFromFactory() {
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
         assertThat(fixture.getPunishments()).isEqualTo(Collections.emptyList());
     }
 
     @Test
-    void shouldMakeBoardBonusFromFactory() {
+    void shouldMakeBoardNoBonusFromFactory() {
+        fixture = boardFactory.createBoard(emptyArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
         assertThat(fixture.getBonus()).isEqualTo(Collections.emptyList());
     }
+
+    @Test
+    void shouldMakeBoardWithBarriersFromFactory() {
+        fixture = boardFactory.createBoard(filledArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
+
+        Position position = Position.builder()
+                                    .x(2)
+                                    .y(0)
+                                    .build();
+
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(3);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(4);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(0);
+        position.setY(1);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(1);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(2);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(3);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(4);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(0);
+        position.setY(2);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(1);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+        position.setX(2);
+        assertThat(fixture.getBarriers()
+                          .contains(position)).isTrue();
+    }
+
+    @Test
+    void shouldMakeBoardWithObjectiveFromFactory()
+    {
+        fixture = boardFactory.createBoard(filledArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
+
+        List<RegularReward> objectives = fixture.getObjectives();
+        assertThat(objectives.get(0).getPosition().getX()).isEqualTo(3);
+        assertThat(objectives.get(0).getPosition().getY()).isEqualTo(2);
+    }
+
+    @Test
+    void shouldMakeBoardWithBonusFromFactory()
+    {
+        fixture = boardFactory.createBoard(filledArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
+
+        List<BonusReward> bonus = fixture.getBonus();
+        assertThat(bonus.get(0).getPosition().getX()).isEqualTo(4);
+        assertThat(bonus.get(0).getPosition().getY()).isEqualTo(2);
+    }
+
+    @Test
+    void shouldMakeBoardWithPunishmentFromFactory()
+    {
+        fixture = boardFactory.createBoard(filledArray2D,
+                                           animateFactory,
+                                           inanimateFactory);
+
+        List<Punishment> punishments = fixture.getPunishments();
+        assertThat(punishments.get(0).getPosition().getX()).isEqualTo(0);
+        assertThat(punishments.get(0).getPosition().getY()).isEqualTo(3);
+    }
+
 
 }
